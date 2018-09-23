0d30ae6e39e8e64f44786b463e5a8a3da87bea75
Serin Delaunay <serin.delaunay@gmail.com>
2018-09-23 22:23:44 +0100

    add Game class

---
+++ game.py
@@ -0,0 +1,68 @@
+from abc import ABCMeta, abstractmethod
+import json
+import re
+from collections import OrderedDict
+
+class Game(metaclass=ABCMeta):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def start_state(self):
+        pass
+    @abstractmethod
+    def options(self, state):
+        pass
+    @abstractmethod
+    def result(self, state, input):
+        pass
+    @abstractmethod
+    def display(self, state):
+        pass
+    @abstractmethod
+    def encode(self, state):
+        pass
+    @abstractmethod
+    def grammar(self):
+        pass
+    @abstractmethod
+    def display_input(self, input):
+        pass
+    def make_grammar(self, filename, state_graph, displays):
+        grammar = self.grammar()
+        for (state_code, options) in state_graph.items():
+            grammar['*'+state_code] = "{}[code:{}][options:{}]#display#".format(displays[state_code], state_code, '‚'.join(k for k in options.keys()))
+        grammar['origin'] = "#*{}#".format(self.encode(self.start_state()))
+        grammar['error'] = "Couldn't understand input. Reply in the format \"\\[code\\] \\[input\\]\"."
+        with open(filename, 'w') as f:
+            json.dump(grammar, f, indent='\t')
+    def make_replies(self, filename, state_graph):
+        states_sorted = sorted(state_graph.keys(), key = len, reverse=True)
+        replies = OrderedDict()
+        for state_code in states_sorted:
+            for (input, result) in state_graph[state_code].items():
+                replies['\\b'+re.escape('{} {}'.format(state_code, input))+'$'] = "{{unlisted}}#*{}#".format(self.encode(result))
+        replies['.'] = "#error#"
+        with open(filename, 'w') as f:
+            json.dump(replies, f, indent='\t')
+    def tracerise(self, filename_base):
+        state_graph = {}
+        displays = {}
+        state_queue = {self.start_state()}
+        while state_queue:
+            state = state_queue.pop()
+            state_code = self.encode(state)
+            state_results = {}
+            if state_code not in state_graph:
+                state_display = self.display(state)
+                displays[state_code] = state_display
+            for option in self.options(state):
+                result_state = self.result(state, option)
+                result_code = self.encode(result_state)
+                input = self.display_input(option)
+                assert input not in state_results, 'input collision'
+                state_results[input] = result_state
+                if result_code not in state_graph:
+                    state_queue.add(result_state)
+            state_graph[state_code] = state_results
+        self.make_grammar(filename_base+'_grammar.json', state_graph, displays)
+        self.make_replies(filename_base+'_replies.json', state_graph)
